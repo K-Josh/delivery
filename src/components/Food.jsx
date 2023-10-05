a41b9be645584f0f4af14bc2b5f7a9b56db3231a
Gaping Joshua <k.j10mars2000@gmail.com>
2023-10-05 14:45:41 +0100

    another commit

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -81,7 +81,7 @@ const Food = () => {
         className='border shadow-lg rounded-lg hover:scale-105'>
             <img src={item.image} alt={item.name} className='w-full h-[200px] object-cover rounded-l-lg'/>
             <div className='lg:flex lg:justify-between px-3 py-4'>
-                <p className='font-bold sm:text-sm'>{item.name}</p>
+                <p className='font-bold text-sm lg:text-md'>{item.name}</p>
                 <p>
                     <span className='bg-secondary p-1 text-sm sm:whitespace-nowrap  rounded-full text-white'>{item.price} </span>
                 </p>
